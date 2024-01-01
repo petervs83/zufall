1367d68b1954cbdb9568868b067e2d4115074c1d
petervs83 <152507995+petervs83@users.noreply.github.com>
2024-01-01 01:05:25 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+	
+})
+for (let index = 0; index < 4; index++) {
+    basic.showString("" + (randint(1, 10)))
+    basic.showString("")
+    basic.showString("" + (randint(0, 100)))
+    basic.pause(100000)
+}
+basic.forever(function () {
+	
+})
